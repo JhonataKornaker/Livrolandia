9a82c282b16277310eae2269b843b977bcf8f60b
Jhonata Kornaker <jhonataalberto@outlook.com>
2025-05-11 20:36:23 -0300

    refactor: ajudar header para nao aparecer em Login e Cadastro.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +7,12 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  constructor(private router: Router){}
+
   title = 'livrolandia';
+
+  get showHeader(): boolean {
+    return this.router.url !== '/';
+  }
 }
