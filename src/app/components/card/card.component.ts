7bc3c3edcdb3bdab643fe5e11e557222d13567f7
Jhonata Kornaker <jhonataalberto@outlook.com>
2025-05-17 15:06:11 -0300

    feat: criar componente card

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+
+  @Input() title: string = 'Titulo do Card';
+  @Input() description: string = 'Descrição do Card';
+  @Input() ano: string = 'Ano';
+  @Input() image?: string;
+  @Input() buttonTextSinopse: string = 'Ver Sinopse';
+  @Input() buttonTextEmprestar: string = 'Emprestar';
+
+  @Output() action = new EventEmitter<void>();
+
+  onAction() {
+    this.action.emit();
+  }
+}
