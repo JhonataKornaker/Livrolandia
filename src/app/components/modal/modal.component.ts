636fccccaeb480ccbc0f0bd10cee87d52e680754
Jhonata Kornaker <jhonataalberto@outlook.com>
2025-05-20 17:28:36 -0300

    feat: criar componente modal para exibir detalhes do card.

---
+++ src/app/components/modal/modal.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.css'
+})
+export class ModalComponent {
+
+  @Input() show: boolean = false;
+  @Output() close = new EventEmitter<void>();
+
+  onClose() {
+    this.close.emit();
+  }
+
+  onOverlayClick(event: MouseEvent) {
+    this.onClose();
+  }
+}
