24ced30e711b47433a8dced80d139f7cbb9bf8d3
Jhonata Kornaker <jhonataalberto@outlook.com>
2025-05-27 17:43:37 -0300

    feat: criar variavel boleana para manipulação de emprestimo de livros.

--- src/app/components/colecao-livro/colecao-livro.component.ts
+++ src/app/components/colecao-livro/colecao-livro.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 import { livros } from '../../data/livros';
 
 @Component({
   selector: 'app-colecao-livro',
   templateUrl: './colecao-livro.component.html',
-  styleUrl: './colecao-livro.component.css'
+  styleUrls: ['./colecao-livro.component.css']
 })
 export class ColecaoLivroComponent {
 
   selectedLivro: any = null;
   isModalOpen = false;
   livros = livros;
+
+  isConfirmModalOpen = false;
+  livroParaConfirmar: any = null;
 
   openModal(livro: any) {
     this.selectedLivro = livro;
@@ -20,5 +22,22 @@ export class ColecaoLivroComponent {
 
   closeModal() {
     this.isModalOpen = false;
+  }
+
+  openConfirmModal(livro: any) {
+    this.livroParaConfirmar = livro;
+    this.isConfirmModalOpen = true;
+  }
+
+  closeConfirmModal() {
+    this.isConfirmModalOpen = false;
+    this.livroParaConfirmar = null;
+  }
+
+  confirmarEmprestimoOuDevolucao() {
+    if (this.livroParaConfirmar) {
+      this.livroParaConfirmar.emprestado = !this.livroParaConfirmar.emprestado;
+    }
+    this.closeConfirmModal();
   }
 }
--- src/app/data/livros.ts
+++ src/app/data/livros.ts
@@ -4,83 +4,95 @@ export const livros = [
     description: 'Stephen King',
     ano: '2018',
     image: 'assets/img_card/img_card_01.png',
-    sinopse: 'Um misterioso assassinato abala a pequena cidade de Castle Rock, enquanto um estranho chamado Ralph questiona sua própria inocência.'
+    sinopse: 'Um misterioso assassinato abala a pequena cidade de Castle Rock, enquanto um estranho chamado Ralph questiona sua própria inocência.',
+    emprestado: false
   },
   {
     title: 'O médico e o monstro',
     description: 'Robert Louis Stevenson',
     ano: '2019',
     image: 'assets/img_card/img_card_02.png',
-    sinopse: 'A luta interna entre o bem e o mal dentro do Dr. Jekyll, que cria uma poção para libertar seu lado mais sombrio, Mr. Hyde.'
+    sinopse: 'A luta interna entre o bem e o mal dentro do Dr. Jekyll, que cria uma poção para libertar seu lado mais sombrio, Mr. Hyde.',
+    emprestado: false
   },
   {
     title: 'Ninguém vai te ouvir gritar',
     description: 'Mark Miller',
     ano: '2023',
     image: 'assets/img_card/img_card_03.png',
-    sinopse: 'Um thriller tenso sobre uma mulher que desaparece misteriosamente, e um detetive que deve desvendar uma teia de segredos sombrios.'
+    sinopse: 'Um thriller tenso sobre uma mulher que desaparece misteriosamente, e um detetive que deve desvendar uma teia de segredos sombrios.',
+    emprestado: false
   },
   {
     title: 'A estranha Sally Diamond',
     description: 'Liz Nugent',
     ano: '2023',
     image: 'assets/img_card/img_card_12.png',
-    sinopse: 'Sally Diamond é uma mulher cheia de segredos e mistérios, cujo passado obscuro ameaça destruir sua vida cuidadosamente construída.'
+    sinopse: 'Sally Diamond é uma mulher cheia de segredos e mistérios, cujo passado obscuro ameaça destruir sua vida cuidadosamente construída.',
+    emprestado: false
   },
   {
     title: 'Introdução à Linguagem SQL',
     description: 'Thomas Nield',
     ano: '2016',
     image: 'assets/img_card/img_card_04.png',
-    sinopse: 'Um guia completo para iniciantes que querem dominar a linguagem SQL e manipular bancos de dados de forma eficiente.'
+    sinopse: 'Um guia completo para iniciantes que querem dominar a linguagem SQL e manipular bancos de dados de forma eficiente.',
+    emprestado: false
   },
   {
     title: 'Arquitetura Limpa',
     description: 'Robert C. Martin',
     ano: '2019',
     image: 'assets/img_card/img_card_05.png',
-    sinopse: 'Este livro aborda princípios e boas práticas para escrever sistemas de software organizados, manuteníveis e escaláveis.'
+    sinopse: 'Este livro aborda princípios e boas práticas para escrever sistemas de software organizados, manuteníveis e escaláveis.',
+    emprestado: false
   },
   {
     title: 'Entendendo Algoritmos',
     description: 'Aditya Y. Bhargava',
     ano: '2017',
     image: 'assets/img_card/img_card_06.png',
-    sinopse: 'Uma introdução visual e didática aos algoritmos fundamentais que todo programador deve conhecer.'
+    sinopse: 'Uma introdução visual e didática aos algoritmos fundamentais que todo programador deve conhecer.',
+    emprestado: false
   },
   {
     title: 'Manual de DevOps',
     description: 'Gene Kim, Jez Humble, John Willis',
     ano: '2018',
     image: 'assets/img_card/img_card_07.png',
-    sinopse: 'Aborda práticas, ferramentas e cultura DevOps para acelerar a entrega de software e melhorar a colaboração entre equipes.'
+    sinopse: 'Aborda práticas, ferramentas e cultura DevOps para acelerar a entrega de software e melhorar a colaboração entre equipes.',
+    emprestado: false
   },
   {
     title: 'A rainha vermelha',
     description: 'Victoria Aveyard',
     ano: '2015',
     image: 'assets/img_card/img_card_08.png',
-    sinopse: 'Em um mundo dividido por sangue, uma jovem descobre ter poderes especiais que podem mudar o destino de sua nação.'
+    sinopse: 'Em um mundo dividido por sangue, uma jovem descobre ter poderes especiais que podem mudar o destino de sua nação.',
+    emprestado: false
   },
   {
     title: 'A cinco passos de você',
     description: 'Rachael Lippincott',
     ano: '2019',
     image: 'assets/img_card/img_card_09.png',
-    sinopse: 'Dois adolescentes com fibrose cística enfrentam o desafio de se apaixonar sem quebrar a regra dos cinco passos de distância.'
+    sinopse: 'Dois adolescentes com fibrose cística enfrentam o desafio de se apaixonar sem quebrar a regra dos cinco passos de distância.',
+    emprestado: false
   },
   {
     title: 'A culpa é das estrelas',
     description: 'John Green',
     ano: '2014',
     image: 'assets/img_card/img_card_10.png',
-    sinopse: 'A história emocionante de dois jovens que lutam contra o câncer e descobrem o verdadeiro significado do amor.'
+    sinopse: 'A história emocionante de dois jovens que lutam contra o câncer e descobrem o verdadeiro significado do amor.',
+    emprestado: false
   },
   {
     title: 'É assim que acaba',
     description: 'Colleen Hoover',
     ano: '2018',
     image: 'assets/img_card/img_card_11.png',
-    sinopse: 'Um romance poderoso que explora os desafios e complexidades de um relacionamento abusivo.'
+    sinopse: 'Um romance poderoso que explora os desafios e complexidades de um relacionamento abusivo.',
+    emprestado: false
   },
 ];
